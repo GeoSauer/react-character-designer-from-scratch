5c2ba7d8ee20e1cff22cfae8121060f1d12eee3b
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-13 10:40:10 -0700

    add Controls basic styling

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -3,5 +3,37 @@ import React from 'react';
 import './Controls.css';
 
 export default function Controls() {
-  return <div>Controls</div>;
+  return (
+    <div className="editor">
+      <div className="form-control">
+        <label htmlFor="head">Head</label>
+        <select>
+          <option>Head 1</option>
+          <option>Head 2</option>
+          <option>Head 3</option>
+        </select>
+      </div>
+      <div className="form-control">
+        <label htmlFor="body">Body</label>
+        <select>
+          <option>Body 1</option>
+          <option>Body 2</option>
+          <option>Body 3</option>
+        </select>
+      </div>
+      <div className="form-control">
+        <label htmlFor="pants">Pants</label>
+        <select>
+          <option>Pants 1</option>
+          <option>Pants 2</option>
+          <option>Pants 3</option>
+        </select>
+      </div>
+      <div className="form-control">
+        <label htmlFor="catchphrase">Add a catch phrase</label>
+        <input type="text" />
+        <button>ADD</button>
+      </div>
+    </div>
+  );
 }
