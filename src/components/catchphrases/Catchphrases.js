61b5d4cc5d23ba0b7e1eb98b4ae10577c6077cf1
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-13 11:42:55 -0700

    add Catchphrase basic styling

--- src/components/catchphrases/Catchphrases.js
+++ src/components/catchphrases/Catchphrases.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 import '../controls/Controls';
+import './Catchphrases.css';
 
 export default function Catchphrases() {
-  return <div className="catchphrases">Catchphrases</div>;
+  return (
+    <div className="catchphrases-container">
+      <ul className="catchphrases">
+        <li>dhgdf</li>
+        <li>hdh</li>
+        <li>hdh</li>
+      </ul>
+    </div>
+  );
 }
