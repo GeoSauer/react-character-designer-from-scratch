b43dff13b9af48454013646ac8aa788361c50299
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-12 18:26:00 -0700

    Character display rough styled

--- src/components/character/Character.js
+++ src/components/character/Character.js
@@ -3,5 +3,11 @@ import React from 'react';
 import './Character.css';
 
 export default function Character() {
-  return <div>Character</div>;
+  return (
+    <div className="character">
+      <div className="display head"></div>
+      <div className="display body"></div>
+      <div className="display pants"></div>
+    </div>
+  );
 }
